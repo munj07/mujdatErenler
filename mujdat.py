d89695cd0fcf14caee757c4c51dee6f7ddd35ec6
munj07 <munj07@gmail.com>
2021-07-04 13:36:10 +0000

    adım6

--- mujdat.py
+++ mujdat.py
@@ -5,12 +5,12 @@ with open('mujdat.json','r') as json_dosya:#mujdat.json dosyası json_dosya olar
     dosya = json.load(json_dosya)#son_dosya yüklenerek dosya değişkenine atandi
 kimlik ={}#kimlik adında bir sözlük oluşturuldu
 #print(dosya)
-for key,value in dosya.items(): #dosyadaki tüm anahtar değer çiftleri için
+#for key,value in dosya.items(): #dosyadaki tüm anahtar değer çiftleri için
     #print(type(dosya.items())) #dosyadan alınan veri tipi kontrol edildi
-    if key == 'kimlik': # anahtar kimlik ise
-        kimlik={}
-        kimlik.update(value) #kimlik sözlüğü json dosyadaki değerler ile güncellendi
+#    if key == 'kimlik': # anahtar kimlik ise
+#        kimlik={}
+#        kimlik.update(value) #kimlik sözlüğü json dosyadaki değerler ile güncellendi
 
-for i in kimlik: #kimlik adlı sözlükteki çiftler için
-    print(kimlik.get(i))# değerler yazdırıldı
-#print("kimlik=",dosya["kimlik"])#mujdat.json dosyasından kimlik anahtarı yazdırıldı
+#for i in kimlik: #kimlik adlı sözlükteki çiftler için
+#    print(kimlik.get(i))# değerler yazdırıldı
+print("kimlik=",dosya["kimlik"])#mujdat.json dosyasından kimlik anahtarı yazdırıldı
